7fcb5c23ada5aeb6bd0670141471fc58eb3a3fc7
sinnisingla <sinnisingla@outlook.com>
2022-04-26 12:50:21 +0530

    Add project files.

---
+++ CacheDemo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CacheDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CacheDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _cache;
+        public HomeController(ILogger<HomeController> logger, IMemoryCache cache)
+        {
+            _logger = logger;
+            this._cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime cacheTimeStamp;
+            if(!_cache.TryGetValue("CachedTimeStamp", out cacheTimeStamp))
+            {
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(10));
+                cacheTimeStamp = DateTime.Now;
+                _cache.Set("CachedTimeStamp", cacheTimeStamp, cacheOptions);
+            }
+            ViewBag.TimeStamp = cacheTimeStamp;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
